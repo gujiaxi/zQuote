b7068496e086117c08cffb319a755bcfbc628cdb
Jiaxi Gu <imjiaxi@gmail.com>
2018-10-26 09:36:42 +0800

    add interactive mode

--- src/zquote.c
+++ src/zquote.c
@@ -10,6 +10,7 @@ int show_help_msg()
     printf("zquote: make fancy quote cards.\n\n");
     printf("Usage:\n\tzquote -b BODY [options]\n");
     printf("Options:\n");
+    printf("\t-i, --interactive       Enter interactive mode.\n");
     printf("\t-b, --body BODY         Specify quote body.\n");
     printf("\t-t, --title TITLE       Specify quote title.\n");
     printf("\t-a, --author AUTHOR     Specify quote author.\n");
@@ -22,21 +23,31 @@ int main(int argc, char *argv[])
     char * body = NULL;
     char * title = NULL;
     char * author = NULL;
+    size_t size;
     while (1) {
         int option_index = 0;
         static struct option long_options[] = {
+            {"interactive", optional_argument, 0, 'i'},
             {"body",    required_argument, 0, 'b' },
             {"title",   required_argument, 0, 't' },
-            {"author",  required_argument, 0, 'a' },
+            {"author",  required_argument, 0, 'a' }
         };
 
-        c = getopt_long(argc, argv, "b:t:a:",
+        c = getopt_long(argc, argv, "ib:t:a:",
                         long_options, &option_index);
         if (c == -1) {
             break;
         }
 
         switch (c) {
+        case 'i':
+            printf("BODY (Required):\n");
+            getline(&body, &size, stdin);
+            printf("TITLE (Optional):\n");
+            getline(&title, &size, stdin);
+            printf("AUTHOR (Optional):\n");
+            getline(&author, &size, stdin);
+            break;
         case 'b':
             body = optarg;
             break;
@@ -52,10 +63,8 @@ int main(int argc, char *argv[])
     }
 
     if (!body) {
-        fprintf(stderr, "Quote body must be specified!!!\n");
         show_help_msg();
         exit(-1);
-    }
+    } else make_quote_img(body, title, author);
 
-    make_quote_img(body, title, author);
 }
